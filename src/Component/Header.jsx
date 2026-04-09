3f775d209615e87a9dac64c1722b2866748f9dc0
Kunalpipaliya <Kunalpipaliya>
2026-04-09 18:32:21 +0530

    implemented UI added some validations for Guest users

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -3,11 +3,17 @@ import { Link } from '@mui/material'
 const Header = () => {
     const currentUser=JSON.parse(localStorage.getItem("currentUser"))
   return (
-    <div className='px-4 py-2  bg-dark text-white'>
+    <div className='px-3 py-2  bg-dark text-white'>
         <div className="d-flex justify-content-between align-items-center ">
                 <h1>SocialMini</h1>
                 
-              {currentUser? <Link href="/notification"><i className="fa-regular fa-heart text-white"></i></Link>:""}
+              {currentUser? <Link href="/notification"><i className="fa-regular fa-heart text-white"></i></Link>:
+              <div className="d-flex gap-2 align-items-center">
+
+              <Link href='/login' className='btn btn-light text-dark text-decoration-none'>Login</Link>
+               <Link href='/signup' className='btn btn-light text-dark text-decoration-none'>Signup</Link>
+              </div>
+               }
         </div>
     </div>
   )
