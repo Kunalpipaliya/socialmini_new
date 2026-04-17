448bbfc879239cbc45541c977b4d3a519127fe4c
Kunalpipaliya <kunalpipaliya67@gmail.com>
2026-04-17 11:47:17 +0530

    nothing

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -4,13 +4,13 @@ import React from 'react'
 const Navbar = () => {
    const currentUser=JSON.parse(localStorage.getItem("currentUser"))
   return (
-    <div className='px-4 py-2 bg-dark' id='nav'>
+    <div className='px-2 py-2 bg-dark' id='nav'>
         <div className="d-flex justify-content-around">
             <Link className='text-decoration-none text-white' href="/"><i className="fa-solid fs-4 fa-house"></i>  </Link>
             <Link className='text-decoration-none text-white' href="/search"><i className="fa-solid fs-4 fa-magnifying-glass"></i>  </Link>
 
             <Link className='text-decoration-none text-white' href={currentUser?"/post":'/login'}><i className="fa-solid fs-4 fa-plus"></i></Link>
-            <Link className='text-decoration-none text-white' href={currentUser?"/chat":'/login'}><i class="fa-regular fs-4 fa-paper-plane"></i></Link>
+            <Link className='text-decoration-none text-white' href={currentUser?"/chat":'/login'}><i className="fa-regular fs-4 fa-paper-plane"></i></Link>
             
             <Link className='text-decoration-none text-white' href={currentUser?"/profile":'/login'}><i className="fa-solid fs-4 fa-circle-user"></i></Link>
         </div>
